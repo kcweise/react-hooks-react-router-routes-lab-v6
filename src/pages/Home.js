ed41f191bd506ca966430c33da2a5086f224be7b
Keenan Weise <kcweise@gmail.com>
2024-03-21 20:12:47 -0500

    Renders Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,36 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard"
 
 function Home() {
+
+  const [movies, setMovies]= useState([]);
+  const url = `http://localhost:4000/movies`
+
+  useEffect(()=> {
+    fetch(url)
+      .then (res => {
+        if (!res.ok) {
+          console.log("Error fetching data");
+        }
+        return res.json();       
+      })
+      .then (data => setMovies(data))
+      .catch(error => console.log("Error fetching data:", error));   
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <h1>Home Page</h1>
       </header>
       <main>
-        {/* Info goes here! */}
+        {movies.map((movie)=>(
+          <MovieCard 
+          key = {movie.id}
+          id = {movie.id}
+          title = {movie.title}
+          />
+        ))}
       </main>
     </>
   );
